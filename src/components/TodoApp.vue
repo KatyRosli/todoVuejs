6f28afb06029dba530b19ed107e059fa22529bbb
Katy Rosli <katyrosli@gmail.com>
2023-02-22 12:11:21 +0100

    started a todoapp with vuejs

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='container'>
+    <h2>My Vue TodoApp</h2>
+
+    <div>
+      <input type='text' placeholder='Enter task'>
+      <button>SUBMIT</button>
+    </div>
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
